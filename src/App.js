68c7d859ebdb39d972dc6343acef709a607acf8c
spmohansp <spmohansp@gmail.com>
2019-09-18 19:27:24 +0530

    index file changes updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,AuthenticatedRoute, Route, Link } from "react-router-dom";
+import Login from './login/index';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <button className="btn btn-primary">Owner Login</button>
+         <button className="btn btn-primary">Manager Login</button>
+
+          <div>You need to <Login />.</div>
+
       </div>
+
+
+
     );
   }
 }
---
+++ src/login/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="App">
+         Hello Login Page
+      </div>
+
+
+
+
+    );
+  }
+}
+
